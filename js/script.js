2fbeecd3f4f1a8063d152c79ddf8b77edb920a33
sosoriov <sebas212@gmail.com>
2015-12-29 14:52:28 -0500

    First commit: initial version

---
+++ js/script.js
@@ -0,0 +1,56 @@
+
+/* ------------------------------------------------------- 
+
+* Filename:     AngularJS Dynamic Form Fields
+* Website:      http://www.shanidkv.com
+* Description:  Shanidkv AngularJS blog
+* Author:       Shanid KV shanidkannur@gmail.com
+
+---------------------------------------------------------*/
+
+var app = angular.module('shanidkvApp', []);
+
+  app.controller('MainCtrl', function($scope, $filter) {
+  $scope.dates = [
+    {month:"December", dayName: "Tuesday", day: "29"},
+    {month:"December", dayName: "Wednesday", day: "30"},
+    {month:"December", dayName: "Thursday", day: "31"},
+    {month:"January", dayName: "Friday", day: "01"},
+    {month:"January", dayName: "Saturday", day: "02"},
+    {month:"January", dayName: "Sunday", day: "03"}
+  ];
+
+  $scope.projects = [
+    {name: "Project 1", imdcNumber: "5538"},
+    {name: "Project TRES", imdcNumber: "0999"},
+    {name: "Rio Bogota", imdcNumber: "H555"},
+    {name: "PIM", imdcNumber: "1852"}
+  ];
+
+  $scope.choices = [];
+  
+  $scope.addNewChoice = function() {
+    var newItemNo = $scope.choices.length+1;
+    $scope.choices.push({'id':'choice'+newItemNo});
+  };
+    
+  $scope.removeChoice = function() {
+    var lastItem = $scope.choices.length-1;
+    $scope.choices.splice(lastItem);
+  };
+
+  function getChoiceId() {
+    angular.forEach($scope.projects, function(project, key) {
+      angular.forEach($scope.dates, function(date, dateKey) {
+        var customId = project["imdcNumber"] + "-" + $filter('limitTo')(date["month"], 3) + "-" + $filter('limitTo')(date["dayName"], 3) + "-" + date["day"];
+        console.log(customId);
+        $scope.choices.push({'id':'choice'+customId});
+        $scope.projects[key]["customId"] = customId;
+
+      });
+    });
+  }
+
+  getChoiceId();
+  
+});
