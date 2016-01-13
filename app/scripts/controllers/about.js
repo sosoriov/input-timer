f19427c9c0b4295bfd20a90efd7f8568152c8bd9
unknown <seo@IMDC-SEO-2.imdc.local>
2016-01-13 18:33:53 -0500

    Initial seed Yeoman Angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inputTimerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the inputTimerApp
+ */
+angular.module('inputTimerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
